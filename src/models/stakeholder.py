42695be012edf07fce0fabdb06fa4ef47d481a7b
Abeeref1 <vip.abeer2005@gmail.com>
2025-05-04 18:17:46 +0400

    Update stakeholder.py

--- src/models/stakeholder.py
+++ src/models/stakeholder.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from src.db import Base
 
 class Stakeholder(Base):
@@ -6,5 +6,6 @@ class Stakeholder(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    contact_email = Column(String, unique=True, index=True, nullable=False)
+    contact_email = Column(String, unique=True, nullable=False, index=True)
     hashed_password = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
