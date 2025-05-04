17c7e2db3ef54d4c8547df8f18130ff6ea8a503b
Abeeref1 <vip.abeer2005@gmail.com>
2025-05-04 17:56:01 +0400

    Update stakeholder.py

--- src/models/stakeholder.py
+++ src/models/stakeholder.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from src.db import Base
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Stakeholder(Base):
     __tablename__ = "stakeholders"
@@ -8,3 +11,10 @@ class Stakeholder(Base):
     name = Column(String, nullable=False)
     contact_email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.hashed_password)
+
+    @classmethod
+    def get_password_hash(cls, password: str) -> str:
+        return pwd_context.hash(password)
