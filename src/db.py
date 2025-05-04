3f7965f9cacfbb2ccab13aaf90ff85ba8b3664d1
Abeeref1 <vip.abeer2005@gmail.com>
2025-05-04 18:19:02 +0400

    Update db.py

--- src/db.py
+++ src/db.py
@@ -2,18 +2,18 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DB URL (uses SQLite by default)
+# --- Database URL
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data.db")
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
-# Engine & session
+# --- Engine & Session
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class for models
+# --- Base class for models
 Base = declarative_base()
 
-# Dependency to get DB session
+# --- Dependency
 def get_db():
     db = SessionLocal()
     try:
