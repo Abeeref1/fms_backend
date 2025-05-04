40209edaddf365689b290a98883197378bd079ed
Abeeref1 <vip.abeer2005@gmail.com>
2025-05-04 18:41:00 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 from src.main import app
 
 if __name__ == "__main__":
-    # Only used if you run "python wsgi.py" locally
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
